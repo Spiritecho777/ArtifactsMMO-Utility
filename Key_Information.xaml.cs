7300ce159f7b3abec3d9cbe820c9fe8fd3dbed32
Asumi <ronan.chaubell@gmail.com>
2024-08-01 17:31:26 +0200

    01/08/2024

---
+++ Key_Information.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ArtifactsMMO_Utility
+{
+    public partial class Key_Information : Window
+    {
+        public Key_Information()
+        {
+            InitializeComponent();
+            if (Properties.Settings.Default.Token != null)
+            {
+                Info_Key.Text = Properties.Settings.Default.Token;
+            }
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Token = Info_Key.Text;
+            Properties.Settings.Default.Save();
+            this.Hide();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,36 +17,73 @@ using System.Windows.Shapes;
 using System.Text.Json;
 using System.Windows.Markup;
 using System.Text.Json.Nodes;
+using System.Security.Policy;
+using System.Runtime.InteropServices;
 
 namespace ArtifactsMMO_Utility
 {
     public partial class MainWindow : Window
     {
-        HttpClient client =new HttpClient();
+        #region Variable
+        HttpClient client = new HttpClient();
+        private Key_Information key_Information = new Key_Information();
+
         private string server = "https://api.artifactsmmo.com";
         private string token;
         private string character;
+        
+        private int PositionX;
+        private int PositionY;
+        
         private bool loopF = false;
-        private bool isRedF =true;
+        private bool isRedF = true;
         private bool loopR = false;
         private bool isRedR = true;
-        private List<Player> player = new List<Player>();
+        private bool FFlag = false;
+        private bool RFlag = false;
 
+        private List<Player> player = new List<Player>();
+        private List<string> itemList = new List<string>();
+        private List<int> itemListCount = new List<int>();
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
-            token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IlNwaXJpdGVjaG8iLCJwYXNzd29yZF9jaGFuZ2VkIjoiIn0.iqT-17qWcSH-dyIAQ-Nu-Fo8E7uzlUhsC7jay2_jDDs";
+
+            if (Properties.Settings.Default.Token != "")
+            {
+                token = Properties.Settings.Default.Token;
+                ConfigureHttpClient();
+                _ = Connect();
+            }
+            else
+            {
+                InfoKey();
+            }
+            //token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IlNwaXJpdGVjaG8iLCJwYXNzd29yZF9jaGFuZ2VkIjoiIn0.iqT-17qWcSH-dyIAQ-Nu-Fo8E7uzlUhsC7jay2_jDDs";
+        }
+
+        private void Key_click(object sender, RoutedEventArgs e)
+        {
+            InfoKey();
+        }
+
+        private void InfoKey()
+        {
+            key_Information.ShowDialog();
+            token = Properties.Settings.Default.Token;
             ConfigureHttpClient();
             _ = Connect();
         }
 
         private void ConfigureHttpClient()
         {
+            System.Console.WriteLine(Properties.Settings.Default.Token);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
 
-        private async Task Connect ()
+        private async Task Connect()
         {
             try
             {
@@ -56,9 +93,9 @@ namespace ArtifactsMMO_Utility
                 string responseBody = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(responseBody);
 
-                using (JsonDocument doc = JsonDocument.Parse(responseBody)) {
+                using (JsonDocument doc = JsonDocument.Parse(responseBody))
+                {
                     JsonElement root = doc.RootElement;
-
                     if (root.TryGetProperty("data", out JsonElement dataElement) && dataElement.ValueKind == JsonValueKind.Array)
                     {
                         foreach (JsonElement element in dataElement.EnumerateArray())
@@ -82,6 +119,7 @@ namespace ArtifactsMMO_Utility
                             }
                         }
                     }
+
                 }
             }
             catch (HttpRequestException ex)
@@ -92,7 +130,7 @@ namespace ArtifactsMMO_Utility
 
         private void Change_Player(object sender, SelectionChangedEventArgs e)
         {
-            if(ListOfPlayer.SelectedItem != null)
+            if (ListOfPlayer.SelectedItem != null)
             {
                 string selectedchanged = ListOfPlayer.SelectedItem.ToString();
 
@@ -114,7 +152,7 @@ namespace ArtifactsMMO_Utility
             {
                 if (ListOfPlayer.SelectedItem.ToString() == sPlayer.PlayerNames)
                 {
-                    if (sPlayer.Task == "none") 
+                    if (sPlayer.Task == "none")
                     {
                         FightLoop.Background = new SolidColorBrush(Colors.Red);
                         RecoltLoop.Background = new SolidColorBrush(Colors.Red);
@@ -124,7 +162,7 @@ namespace ArtifactsMMO_Utility
                         FightLoop.Background = new SolidColorBrush(Colors.Green);
                         RecoltLoop.Background = new SolidColorBrush(Colors.Red);
                     }
-                    if(sPlayer.Task == "recolt")
+                    if (sPlayer.Task == "recolt")
                     {
                         FightLoop.Background = new SolidColorBrush(Colors.Red);
                         RecoltLoop.Background = new SolidColorBrush(Colors.Green);
@@ -133,7 +171,158 @@ namespace ArtifactsMMO_Utility
             }
         }
 
-        private async void FightLoop_Click(object sender, RoutedEventArgs e)
+        private void FightLoop_Click(object sender, RoutedEventArgs e)
+        {
+            FLoop();
+        }
+
+        private void RecolteLoop_Click(object sender, RoutedEventArgs e)
+        {
+            RLoop();
+        }
+
+        private async void VenteAuto()
+        {
+            #region Recuperation de position
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(server + "/my/characters");
+                response.EnsureSuccessStatusCode(); // Vérifie que la requête a réussi
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+
+                using (JsonDocument doc = JsonDocument.Parse(responseBody))
+                {
+                    JsonElement root = doc.RootElement;
+
+                    if (root.TryGetProperty("data", out JsonElement dataElement) && dataElement.ValueKind == JsonValueKind.Array)
+                    {
+                        foreach (JsonElement element in dataElement.EnumerateArray())
+                        {
+                            if (element.TryGetProperty("x", out JsonElement accountElement))
+                            {
+                                PositionX = accountElement.GetInt32();
+                                break;
+                            }
+                        }
+                        foreach (JsonElement element in dataElement.EnumerateArray())
+                        {
+                            if (element.TryGetProperty("y", out JsonElement accountElement))
+                            {
+                                PositionY = accountElement.GetInt32();
+                                break;
+                            }
+                        }
+                        foreach (JsonElement element in dataElement.EnumerateArray())
+                        {
+                            for (int i = 1; i < 20; i++)
+                            {
+                                if (element.TryGetProperty($"inventory_slot{i}", out JsonElement accountElement))
+                                {
+                                    itemList.Add(accountElement.ToString());                                
+                                }
+                                if (element.TryGetProperty($"inventory_slot{i}_quantity", out JsonElement accountElement2))
+                                {
+                                    itemListCount.Add(accountElement2.GetInt32());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Erreur lors de la requête: {ex.Message}");
+            }
+            #endregion
+
+            #region Déplacement
+            string url = $"{server}/my/{character}/action/move";
+            var options = new StringContent("{\n  \"x\": 5,\n  \"y\": 1\n}");
+
+            string jsonBody = JsonSerializer.Serialize(options);
+            var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = content
+            };
+
+            try
+            {
+                HttpResponseMessage response = await client.SendAsync(requestMessage);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException fe)
+            {
+                Console.WriteLine("Request error: " + fe.Message);
+            }
+            #endregion
+
+            #region Vente
+            url = $"{server}/my/{character}/action/ge/sell";
+            try
+            {
+                for (int i =0; i < itemList.Count; i++)
+                {
+                    var item = new StringContent($"{{\"code\":\"{itemList[i]}\",\"quantity\":{itemListCount[i]},\"price\":1}}");
+                    HttpResponseMessage response = await client.PostAsync(url, item);
+
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+            }
+            catch (HttpRequestException fe)
+            {
+                Console.WriteLine("Request error: " + fe.Message);
+            }
+            #endregion
+
+            #region Retour
+            url = $"{server}/my/{character}/action/move";
+            options = new StringContent($"{{\n  \"x\": \"{PositionX}\",\n  \"y\": \"{PositionY}\"\n}}");
+
+            jsonBody = JsonSerializer.Serialize(options);
+            content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            requestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = content
+            };
+
+            try
+            {
+                HttpResponseMessage response = await client.SendAsync(requestMessage);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException fe)
+            {
+                Console.WriteLine("Request error: " + fe.Message);
+            }
+            #endregion
+
+            itemList.Clear();
+            itemListCount.Clear();
+
+            if (FFlag == true)
+            {
+                FFlag = false;
+                FLoop();
+            }
+            if (RFlag == true)
+            {
+                RFlag = false;
+                RLoop();
+            }
+        }
+
+        private async void FLoop()
         {
             loopF = !loopF;
             FightLoop.Background = new SolidColorBrush(isRedF ? Colors.Green : Colors.Red);
@@ -153,7 +342,6 @@ namespace ArtifactsMMO_Utility
             {
                 try
                 {
-                    // Send POST request
                     HttpResponseMessage response = await client.PostAsync(url, null);
 
                     if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized) // 498
@@ -164,6 +352,11 @@ namespace ArtifactsMMO_Utility
                     else if (response.StatusCode == (System.Net.HttpStatusCode)497)
                     {
                         Logs.Content = ("Your character's inventory is full.");
+                        if (AutoCheck.IsChecked == true)
+                        {
+                            FFlag = true;
+                            VenteAuto();
+                        }
                         return;
                     }
                     else if (response.StatusCode == (System.Net.HttpStatusCode)499)
@@ -225,17 +418,18 @@ namespace ArtifactsMMO_Utility
             Logs.Content = "You have stop";
         }
 
-        private async void RecolteLoop(object sender, RoutedEventArgs e)
+        private async void RLoop()
         {
             loopR = !loopR;
             RecoltLoop.Background = new SolidColorBrush(isRedR ? Colors.Green : Colors.Red);
             isRedR = !isRedR;
 
-            foreach (var sPlayer in player) {
+            foreach (var sPlayer in player)
+            {
                 if (ListOfPlayer.SelectedItem.ToString() == sPlayer.PlayerNames)
                 {
                     sPlayer.Task = "recolt";
-                } 
+                }
             }
 
             string url = $"{server}/my/{character}/action/gathering";
@@ -244,7 +438,6 @@ namespace ArtifactsMMO_Utility
             {
                 try
                 {
-                    // Send POST request
                     HttpResponseMessage response = await client.PostAsync(url, null);
 
                     if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized) // 498
@@ -255,6 +448,11 @@ namespace ArtifactsMMO_Utility
                     else if (response.StatusCode == (System.Net.HttpStatusCode)497)
                     {
                         Logs.Content = ("Your character's inventory is full.");
+                        if (AutoCheck.IsChecked == true)
+                        {
+                            RFlag = true;
+                            VenteAuto();
+                        }
                         return;
                     }
                     else if (response.StatusCode == (System.Net.HttpStatusCode)499)
@@ -316,3 +514,16 @@ namespace ArtifactsMMO_Utility
         }
     }
 }
+
+/*
+                using (JsonDocument doc = JsonDocument.Parse(response))
+                {
+                JsonElement root = doc.RootElement;
+
+                // Accéder aux données
+                var cooldownSeconds = root.GetProperty("data").GetProperty("cooldown").GetProperty("totalSeconds").GetInt32();
+
+                // Attendre la fin du cooldown
+                await Task.Delay(cooldownSeconds * 1000);
+                }
+*/
